d44941e08f839dbd99b31c8065f75ed45f559e8e
Arnab Modak <arnab.modak.kol.2002@gmail.com>
2024-08-31 20:37:23 +0530

    Clarity Fixed

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./global.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,6 +22,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ntwukzde5w");
+          `}
+        </Script>
     </html>
   );
 }
